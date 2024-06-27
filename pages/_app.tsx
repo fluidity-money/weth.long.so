09a54045e06d66d8468cced026834e4b03ac812b
bayge <alex@fluidity.money>
2024-06-27 11:59:44 +0930

    Initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,27 +4,31 @@ import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
-import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
-} from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 
 const config = getDefaultConfig({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
-  ],
+  appName: 'SPN Wrapper',
+  projectId: `${process.env.NEXT_PUBLIC_WSPN_WALLETCONNECT_PROJECT_ID}`,
+  chains: [{
+    name: "Superposition Testnet",
+    id: 98985,
+    nativeCurrency: { name: "Superposition", symbol: "SPN", decimals: 18 },
+    contracts: {
+      multicall3: {
+        address: "0x37Bc0f77FCf51318B2ceb9447002913f7CFb599d" as const,
+      },
+    },
+    rpcUrls: {
+      default: { http: ["https://testnet-rpc.superposition.so"] },
+      public: { http: ["https://testnet-rpc.superposition.so"] },
+    },
+    blockExplorers: {
+      default: {
+        name: "CatScan",
+        url: "https://testnet-explorer.superposition.so",
+      },
+    },
+  }],
   ssr: true,
 });
 
